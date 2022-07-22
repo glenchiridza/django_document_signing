cbacd99782ebef57b738a7aaa0f75a1ea120ee34
glenchiridza <glenchiridz@gmail.com>
2022-07-23 00:49:33 +0200

    document model

--- files/models.py
+++ files/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    document_name = models.CharField(max_length=100)
+    upload_pdf = models.FileField(upload_to="pdf_documents")
+
+    def __str__(self):
+        return self.document_name
