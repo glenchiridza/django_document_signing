0456004797f7bec31f1d16a19b13b98e67e32974
glenchiridza <glenchiridz@gmail.com>
2022-07-23 03:06:49 +0200

    sign document

--- files/admin.py
+++ files/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Document, Signature
+from .models import Document, Signature,SignDocument
 
 admin.site.register(Signature)
+# admin.site.register(SignDocument)
 admin.site.register(Document)
