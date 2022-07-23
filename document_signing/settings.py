1ea15bdf903727573680835a08e4bc2404203fe7
glenchiridza <glenchiridz@gmail.com>
2022-07-23 20:35:26 +0200

    update styling

--- document_signing/settings.py
+++ document_signing/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'document_signing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = 'static_root'
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
