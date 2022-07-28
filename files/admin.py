ce7a23016d0a4bc8e18453030a8bc05bc72fd99b
glenchiridza <glenchiridz@gmail.com>
2022-07-29 00:02:52 +0200

    show options admin panel

--- files/admin.py
+++ files/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Document, Signature,SignDocument, ESignModel,ESignDocument,SendForSigning
 
 admin.site.register(Signature)
-admin.site.register(SignDocument)
-admin.site.register(Document)
+# admin.site.register(SignDocument)
+# admin.site.register(Document)
 admin.site.register(ESignModel)
-admin.site.register(ESignDocument)
-admin.site.register(SendForSigning)
+# admin.site.register(ESignDocument)
+# admin.site.register(SendForSigning)
